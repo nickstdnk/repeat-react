814644d6f51830f4255f8c06a3e5940e1cb5b02a
nickstdnk <nick.stdnk@gmail.com>
2022-04-02 18:27:16 +0300

    fix

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <>
+      <div className={styles.header}>
+        <header>
+          <img src="https://freelogodesign.org/Content/img/logo-ex-7.png" alt=""/>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default Header
