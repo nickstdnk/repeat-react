1eb79fe1c36301ebc6e64c087cc87c804731145e
nickstdnk <nick.stdnk@gmail.com>
2022-04-24 00:38:22 +0300

    refactor SideBar

--- src/layout/Sidebar/SidebarContainer.jsx
+++ src/layout/Sidebar/SidebarContainer.jsx
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux'
 import Sidebar from './Sidebar'
 
+const SidebarContainer = (props) => {
+  return (
+    <Sidebar {...props}/>
+  )
+}
+
 let mapStateToProps = (state) => {
   return {
     friends: state.friendsPage.friends,
     menu: state.sidebar.menu,
   }
 }
+export default connect(mapStateToProps)(SidebarContainer)
 
-const SidebarContainer = connect(mapStateToProps, null)(Sidebar)
 
-export default SidebarContainer
